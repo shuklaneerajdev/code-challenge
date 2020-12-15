f01daa4e88fd457e13b47db4f7c7b8a56a07356f
Shukla <shklnrj@gmail.com>
2020-12-15 21:43:41 +0530

    Added server side validation for emails used for company.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,14 @@
 class Company < ApplicationRecord
   has_rich_text :description
+  validate :company_email_if_present
 
+  def company_email_if_present
+    if email.present?
+      details = email.split('@')
+      if !(details.size ==2 && details.last == "getmainstreet.com")
+        errors.add(:email, "Is invalid or using non getmainstreet.com email address")
+      end
+    end
+  end
 
 end
